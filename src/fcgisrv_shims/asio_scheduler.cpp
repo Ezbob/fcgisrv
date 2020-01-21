3bbbb3158c719bd729915e5dd6b429fb9b7b89b8
Anders Busch <andersbusch@gmail.com>
2020-01-21 20:59:43 +0100

    removed shims

--- src/fcgisrv_shims/asio_scheduler.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "fcgisrv_shims/asio_scheduler.hpp"
-
-using namespace fcgisrv;
-
-void fcgisrv::AsioScheduler::schedule_task(std::function<void()> const& task) {
-    m_io_ctx.post(task);
-}
